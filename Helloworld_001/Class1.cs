e65b2e345683a7fd31af2b530fdb9eb2b0a739a3
duongnguyenhong2004 <duongnguyenhong2004@gmail.com>
2025-03-27 13:21:20 +0700

    Add project files.

---
+++ Helloworld_001/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Forms;
+
+namespace Helloworld_001
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            ICollection<ElementId> selectElements = uidoc.Selection.GetElementIds();
+
+            int totalCount = selectElements.Count;
+            MessageBox.Show(" ban da chon " + totalCount.ToString() + " elements");
+            return Result.Succeeded;
+        }
+    }
+}
